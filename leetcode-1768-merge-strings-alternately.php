3bcc8a66bdb38165f7430c596b90624757bf78f4
shyiran <shyiran123@163.com>
2022-12-03 23:55:05 +0800

    补充

--- leetcode-1768-merge-strings-alternately.php
+++ leetcode-1768-merge-strings-alternately.php
@@ -1,8 +1,15 @@
+<?php
+/*
+1768. 交替合并字符串
+简单
+74
+相关企业
+
 给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串。如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
 
 返回 合并后的字符串 。
 
- 
+ 
 
 示例 1：
 
@@ -18,22 +25,41 @@ word2：    p   q   r
 输入：word1 = "ab", word2 = "pqrs"
 输出："apbqrs"
 解释：注意，word2 比 word1 长，"rs" 需要追加到合并后字符串的末尾。
-word1：  a   b 
+word1：  a   b
 word2：    p   q   r   s
 合并后：  a p b q   r   s
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/merge-strings-alternately
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+示例 3：
+
+输入：word1 = "abcd", word2 = "pq"
+输出："apbqcd"
+解释：注意，word1 比 word2 长，"cd" 需要追加到合并后字符串的末尾。
+word1：  a   b   c   d
+word2：    p   q
+合并后：  a p b q c   d
+
+ 
+
+提示：
+
+    1 <= word1.length, word2.length <= 100
+    word1 和 word2 由小写英文字母组成
 
 
 
 
+https://leetcode.cn/problems/merge-strings-alternately/
+*/
 
 
-
- function mergeAlternately($word1, $word2)
-    {
+class Solution1768
+{
+    /**
+     * @param String $word1
+     * @param String $word2
+     * @return String
+     */
+    function mergeAlternately(string $word1, string $word2):string {
         $res = '';
         $max = max(strlen($word1), strlen($word2));
         for ($i = 0; $i < $max; $i++) {
@@ -41,8 +67,4 @@ word2：    p   q   r   s
         }
         return $res;
     }
-
-作者：lupin86
-链接：https://leetcode-cn.com/problems/merge-strings-alternately/solution/php-forxun-huan-6xing-dai-ma-qiu-jie-by-e9l13/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
