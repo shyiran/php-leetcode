b93b4cf7ce4708b234258cc87479d41f1af7ce8e
shyiran <shyiran123@163.com>
2022-12-05 19:07:57 +0800

    补充

---
+++ leetcode-003-longest-substring-without-repeating-characters(1).php
@@ -0,0 +1,65 @@
+<?php
+/*
+003.给定一个字符串，请你找出其中不含有重复字符的 **最长子串** 的长度。
+提示：
+- 0 <= s.length <= 5 * 10<sup>4</sup>
+- s 由英文字母、数字、符号和空格组成
+
+示例 1：
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+
+示例 2：
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+
+示例 3：
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。
+    请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
+
+## 滑动窗口
+
+&emsp;&emsp;维护一个滑动窗口，窗口内的都是没有重复的字符，去尽可能的扩大窗口的大小，窗口不停的向右滑动。
+
+1. 如果当前遍历到的字符从未出现过，那么直接扩大右边界；
+2. 如果当前遍历到的字符出现过，则缩小窗口（左边索引向右移动），然后继续观察当前遍历到的字符；
+3. 重复上面两步，直到左边索引无法再移动；
+4. 维护一个结果 max，每次用出现过的窗口大小来更新结果 max，最后返回 max 获取结果。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class LeetCode0003
+{
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLongestSubstring(string $s):int {
+        //定义一个脸是变量
+        $str = "";
+        $len = 0;
+        $s_l= strlen($s);
+        for($i=0;$i<$s_l;$i++){
+            //是否包含在临时字符串
+            $isBaohan = strpos($str,$s[$i]);
+            if($isBaohan === false){
+                $str .= $s[$i];
+            }else{
+                $str.= $s[$i];
+                //abcabcbb  比如到了第二个a之后
+                $str = substr($str,$isBaohan+1);
+            }
+            $len =  strlen($str)>$len ? strlen($str):$len;
+        }
+        return $len;
+    }
+}
